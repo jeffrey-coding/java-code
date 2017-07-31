633d0ee7c6ad2e62074cfcd9e023fd7f1d7ab581
jeffrey sun <jeffrey.coding@foxmail.com>
2017-07-31 23:11:48 +0800

    完成 习题1-2

--- src/main/java/me/jeffrey/algorithms/base/chapter2/InsertSort.java
+++ src/main/java/me/jeffrey/algorithms/base/chapter2/InsertSort.java
@@ -1,8 +1,6 @@
 package me.jeffrey.algorithms.base.chapter2;
 
 
-import java.util.Arrays;
-
 public class InsertSort {
     public static void insertSort(Comparable[] input){
         long len = input.length;
@@ -15,13 +13,22 @@ public class InsertSort {
             }
             input[i+1] = key;
         }
-        System.out.println(Arrays.toString(input));
+
     }
 
-    public static void main(String[] args) {
-        Integer[] test = {2,9,3,6,1};
-        System.out.println(Arrays.toString(test));
-        InsertSort.insertSort(test);
-        System.out.println("====end=====");
+    public static void insertSortDesc(Comparable[] input){
+        long len = input.length;
+        for(int j = 1; j < len; j++){
+            Comparable key = input[j];
+            int i = j - 1;
+            while (i >= 0 && input[i].compareTo(key) < 0){
+                input[i+1] = input[i];
+                i = i - 1;
+            }
+            input[i+1] = key;
+        }
+
     }
+
+
 }
--- src/test/java/me/jeffrey/algorithms/base/chapter2/InsertSortTest.java
+++ src/test/java/me/jeffrey/algorithms/base/chapter2/InsertSortTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.After;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /** 
 * InsertSort Tester. 
@@ -14,27 +15,51 @@ import java.util.Arrays;
 * @version 1.0 
 */ 
 public class InsertSortTest {
+    Random random ;
+    Integer[] test;
 
     @Before
     public void before() throws Exception {
+        random = new Random();
+        test = new Integer[10];
+        for (int i = 0; i < 10; i++) {
+            test[i] = random.nextInt(1000);
+        }
     }
 
     @After
     public void after() throws Exception {
+        System.out.println("test end");
     }
 
     /**
+     * 测试插入排序 升序
      * Method: insertSort(Comparable[] input)
      */
     @Test
     public void testInsertSort() throws Exception {
-        Integer[] test = {2, 9, 3, 6, 1};
         System.out.println(Arrays.toString(test));
         InsertSort.insertSort(test);
-        System.out.println("====end=====");
+        System.out.println(Arrays.toString(test));
         for (int i = 0; i < test.length-1; i++) {
             assert test[i] <= test[i+1];
         }
     }
+
+/**
+*
+* Method: insertSortDesc(Comparable[] input)
+*
+*/
+@Test
+public void testInsertSortDesc() throws Exception {
+    System.out.println(Arrays.toString(test));
+    InsertSort.insertSortDesc(test);
+    System.out.println(Arrays.toString(test));
+    for (int i = 0; i < test.length-1; i++) {
+        assert test[i] >= test[i+1];
+    }
+}
+
 
 }
